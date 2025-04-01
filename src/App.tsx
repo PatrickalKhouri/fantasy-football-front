dc6fff9c663775f5358ed3bcec4156dfb29d720d
Patrick Khouri <patrick.darya@gmail.com>
2025-04-01 11:34:41 -0300

    Did the top navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,16 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { Box, Button } from "@mui/material";
+import { BrowserRouter as Router } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
-function App() {
+const App: React.FC = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <Box sx={{ backgroundColor: "primary.main", padding: 20 }}>
-          <Button sx={{ color: "white", bgcolor: "secondary.main" }}>
-            Click Me
-          </Button>
-        </Box>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <Navbar />
+            Fantasy Brasileirao
+      </div>
+    </Router>
   );
-}
+};
 
 export default App;
