5141fbbb1fc5e399ee6c55164733f16f4ef5b96f
Patrick Khouri <patrick.darya@gmail.com>
2025-04-02 22:18:50 -0300

    formatted the sign in page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import SignIn from './pages/SignIn';
+import SignUp from './pages/SignUp';
 
 const App: React.FC = () => {
   return (
     <Router>
       <Routes>
         <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
         <Route path="/" element={
           <div className="App">
             <Navbar />
